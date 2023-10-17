2554c7cc9b5bc511a8764b97ba80a2ddc91dbc7c
khmaladze <khmaladze.g.7@gmail.com>
2023-10-16 23:43:05 -0700

    update profile page

--- frontend/src/pages/profile-page/ProfilePage.jsx
+++ frontend/src/pages/profile-page/ProfilePage.jsx
@@ -33,6 +33,10 @@ import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
 // Import the plugin code
 import FilePondPluginFileValidateSize from "filepond-plugin-file-validate-size";
 import { Grid } from "@mui/material";
+import InfoIcon from "@mui/icons-material/Info";
+import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
+import { education, languages, passions } from "../../data/userInfoData";
+import { toast } from "react-toastify";
 
 // Register the plugins
 registerPlugin(
@@ -49,31 +53,63 @@ const ProfilePage = ({ user }) => {
     ) || null;
 
   const [image, setImage] = useState();
+  const [selectLanguage, setselectLanguage] = useState("");
+  const [selectEducation, setselectEducation] = useState("");
+  const [selectPassion, setselectPassion] = useState("");
 
   const updateuserprofileimageRequestUrl =
     API_URL.user.user_profile.update_profile_image;
 
   const updateUserImageHandle = async () => {
-    const formData = new FormData();
-    formData.append("image", image[0]["file"]);
-    const response = await apiPutRequest(
-      updateuserprofileimageRequestUrl,
-      formData,
-      user.token,
-      API_CONTENT_TYPE.update_user_profile_image
-    );
-
-    if (response.success) {
-      const userProfileData = JSON.parse(
-        localStorage.getItem(userLocalstorage.auth.user)
+    if (image && image[0] && image[0]["file"]) {
+      const formData = new FormData();
+      formData.append("image", image[0]["file"]);
+      const response = await apiPutRequest(
+        updateuserprofileimageRequestUrl,
+        formData,
+        user.token,
+        API_CONTENT_TYPE.update_user_profile_image
       );
 
-      localStorage.removeItem(userLocalstorage.auth.user);
-      setLocalstorage(userLocalstorage.auth.user, {
-        ...userProfileData,
-        ...response.data,
-      });
-      window.location.reload();
+      if (response.success) {
+        const userProfileData = JSON.parse(
+          localStorage.getItem(userLocalstorage.auth.user)
+        );
+
+        localStorage.removeItem(userLocalstorage.auth.user);
+        setLocalstorage(userLocalstorage.auth.user, {
+          ...userProfileData,
+          ...response.data,
+        });
+        window.location.reload();
+      }
+    } else {
+      toast.error("please add image");
+    }
+  };
+
+  const updateUserInfoHandle = async () => {
+    if (!selectLanguage || !selectPassion || !selectEducation) {
+      toast.error("please add all the fields");
+    } else {
+      const postData = {
+        languages: [selectLanguage],
+        passions: [selectPassion],
+        education: selectEducation,
+      };
+
+      const updateuserprofileinfodataRequestUrl =
+        API_URL.user.user_profile.update_user_profile_info_data;
+      const response = await apiPutRequest(
+        updateuserprofileinfodataRequestUrl,
+        postData,
+        user.token
+      );
+
+      if (response?.success) {
+        localStorage.removeItem(userLocalstorage.auth.userProfileInfoData);
+        window.location.reload();
+      }
     }
   };
 
@@ -97,12 +133,23 @@ const ProfilePage = ({ user }) => {
     }
   }, []);
 
+  const handleLanguageChange = (event) => {
+    setselectLanguage(event.target.value);
+  };
+
+  const handleEducationChange = (event) => {
+    setselectEducation(event.target.value);
+  };
+
+  const handlePassionChange = (event) => {
+    setselectPassion(event.target.value);
+  };
+
   return (
     <ProfilePageMain>
       <ProfileInfoContainer>
         <ProfileImage image={userProfileImage(user.gender, user.profileImage)}>
           <MyModal
-            title={"Update Profile Image"}
             ButtonText={
               <Button
                 variant="contained"
@@ -112,6 +159,7 @@ const ProfilePage = ({ user }) => {
                 Update Image
               </Button>
             }
+            title={"Update Profile Image"}
             body={
               <div>
                 <Grid item xs={12}>
@@ -151,6 +199,98 @@ const ProfilePage = ({ user }) => {
                 <h3>passion: {userProfileData?.passions[0]}</h3>
               </Fragment>
             )}
+            <MyModal
+              title={"Update Info"}
+              ButtonText={
+                <Button
+                  style={{ width: "100%", marginTop: "10px" }}
+                  variant="contained"
+                  color="primary"
+                  startIcon={<InfoIcon />}
+                >
+                  Update Info
+                </Button>
+              }
+              body={
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <FormControl
+                      style={{
+                        width: "100%",
+                      }}
+                    >
+                      <InputLabel id="dropdown-label">Language</InputLabel>
+                      <Select
+                        labelId="dropdown-label"
+                        id="dropdown"
+                        value={selectLanguage}
+                        onChange={handleLanguageChange}
+                      >
+                        {languages.map((item) => {
+                          return (
+                            <MenuItem key={item} value={item}>
+                              {item}
+                            </MenuItem>
+                          );
+                        })}
+                      </Select>
+                    </FormControl>
+                    <FormControl
+                      style={{
+                        width: "100%",
+                        marginTop: "10px",
+                      }}
+                    >
+                      <InputLabel id="dropdown-label">degree</InputLabel>
+                      <Select
+                        labelId="dropdown-label"
+                        id="dropdown"
+                        value={selectEducation}
+                        onChange={handleEducationChange}
+                      >
+                        {education.map((item) => {
+                          return (
+                            <MenuItem key={item} value={item}>
+                              {item}
+                            </MenuItem>
+                          );
+                        })}
+                      </Select>
+                    </FormControl>
+                    <FormControl
+                      style={{
+                        width: "100%",
+                        marginTop: "10px",
+                      }}
+                    >
+                      <InputLabel id="dropdown-label">passion</InputLabel>
+                      <Select
+                        labelId="dropdown-label"
+                        id="dropdown"
+                        value={selectPassion}
+                        onChange={handlePassionChange}
+                      >
+                        {passions.map((item) => {
+                          return (
+                            <MenuItem key={item} value={item}>
+                              {item}
+                            </MenuItem>
+                          );
+                        })}
+                      </Select>
+                    </FormControl>
+                    <Button
+                      style={{ width: "100%", marginTop: "10px" }}
+                      variant="contained"
+                      color="primary"
+                      onClick={updateUserInfoHandle}
+                    >
+                      Update
+                    </Button>
+                  </Grid>
+                </Grid>
+              }
+            />
           </div>
         </ProfileDetails>
       </ProfileInfoContainer>
