c52f57c3200ac171178aaca40c6cddad964f35c0
khmaladze <khmaladze.g.7@gmail.com>
2023-10-20 21:33:59 -0700

    add UserPostStyle

---
+++ frontend/src/components/post/UserPostStyle.jsx
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+
+export const UserPostContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
+`;
+
+export const UserPostDiv = styled.div`
+  max-width: 700px;
+  width: 700px;
+  min-height: 350px;
+  border: 3px solid #1eff1e;
+  background: white;
+  border-radius: 15px;
+  transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+`;
+
+export const UserPostHeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-width: 170px;
+`;
+
+export const UserPostHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 70px;
+  width: 100%;
+  border-bottom: 3px solid #1eff1e;
+  padding: 10px;
+`;
+
+export const UserPostBodyText = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  min-width: 170px;
+  padding: 10px;
+`;
+
+export const UserPostBodyImage = styled.div`
+  ${(props) => (props.primary ? "blue" : "gray")};
+  width: 100%;
+  height: 100%;
+  min-height: 350px;
+  max-height: 500px;
+  background-size: contain;
+  background-image: url(${(props) => props.image});
+  background-position: center;
+  background-repeat: no-repeat;
+`;
+
+export const UserPostFooter = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 50px;
+  width: 100%;
+  border-top: 3px solid #1eff1e;
+  padding: 10px;
+`;
