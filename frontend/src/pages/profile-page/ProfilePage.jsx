6498e2a1e2b1b86b3d5707fd678174c1f203ea1b
khmaladze <khmaladze.g.7@gmail.com>
2023-10-21 12:19:33 -0700

    update ProfilePage

--- frontend/src/pages/profile-page/ProfilePage.jsx
+++ frontend/src/pages/profile-page/ProfilePage.jsx
@@ -1,7 +1,6 @@
 import React, { Fragment, useEffect, useState } from "react";
 import {
   ProfileDetails,
-  ProfileImage,
   ProfileInfoContainer,
   ProfilePageMain,
 } from "./ProfilePageStyle";
@@ -10,38 +9,19 @@ import {
   apiPutRequest,
   setLocalstorage,
   userLocalstorage,
-  userProfileImage,
 } from "../../api/Api";
-import { API_CONTENT_TYPE_LIST, API_URL } from "../../config/config";
+import { API_URL } from "../../config/config";
 import UserPost from "../../components/post/UserPost";
 import Button from "@mui/material/Button";
-import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 import MyModal from "../../components/modal/MyModal";
-// Import React FilePond
-import { FilePond, registerPlugin } from "react-filepond";
-// Import FilePond styles
-import "filepond/dist/filepond.min.css";
-import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
-import FilePondPluginImagePreview from "filepond-plugin-image-preview";
-import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
-// Import the plugin code
-import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
-// Import the plugin code
-import FilePondPluginFileValidateSize from "filepond-plugin-file-validate-size";
 import { Grid } from "@mui/material";
 import InfoIcon from "@mui/icons-material/Info";
 import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 import { education, languages, passions } from "../../data/userInfoData";
 import { toast } from "react-toastify";
 import AddPost from "../../components/post/AddPost";
+import ProfileImageComponent from "../../components/profile-page/ProfileImage";
 
-// Register the plugins
-registerPlugin(
-  FilePondPluginFileValidateSize,
-  FilePondPluginImageExifOrientation,
-  FilePondPluginImagePreview,
-  FilePondPluginFileValidateType
-);
 const ProfilePage = ({ user }) => {
   const [userProfileData, setUserProfileData] = useState("");
   const userProfileInfoData =
@@ -49,7 +29,6 @@ const ProfilePage = ({ user }) => {
       localStorage.getItem(userLocalstorage.auth.userProfileInfoData)
     ) || null;
 
-  const [image, setImage] = useState();
   const [selectLanguage, setselectLanguage] = useState(
     userProfileInfoData ? userProfileInfoData.languages[0] : ""
   );
@@ -60,34 +39,6 @@ const ProfilePage = ({ user }) => {
     userProfileInfoData ? userProfileInfoData.passions[0] : ""
   );
   const [profilePosts, setProfilePosts] = useState("");
-
-  const updateUserImageHandle = async () => {
-    if (image && image[0] && image[0]["file"]) {
-      const formData = new FormData();
-      formData.append("image", image[0]["file"]);
-      const response = await apiPutRequest(
-        API_URL.updateUserProfileImageRequestUrl,
-        formData,
-        user.token,
-        API_CONTENT_TYPE_LIST.application_x_www_form_urlencoded
-      );
-
-      if (response.success) {
-        const userProfileData = JSON.parse(
-          localStorage.getItem(userLocalstorage.auth.user)
-        );
-
-        localStorage.removeItem(userLocalstorage.auth.user);
-        setLocalstorage(userLocalstorage.auth.user, {
-          ...userProfileData,
-          ...response.data,
-        });
-        window.location.reload();
-      }
-    } else {
-      toast.error("please add image");
-    }
-  };
 
   const updateUserInfoHandle = async () => {
     if (!selectLanguage || !selectPassion || !selectEducation) {
@@ -161,45 +112,7 @@ const ProfilePage = ({ user }) => {
   return (
     <ProfilePageMain>
       <ProfileInfoContainer>
-        <ProfileImage image={userProfileImage(user.gender, user.profileImage)}>
-          <MyModal
-            ButtonText={
-              <Button
-                variant="contained"
-                color="primary"
-                startIcon={<AddPhotoAlternateIcon />}
-              >
-                Update Image
-              </Button>
-            }
-            title={"Update Profile Image"}
-            body={
-              <div>
-                <Grid item xs={12}>
-                  <FilePond
-                    files={image}
-                    allowMultiple={false}
-                    maxFiles={1}
-                    onupdatefiles={setImage}
-                    allowFileSizeValidation={true}
-                    maxFileSize={"5MB"}
-                    acceptedFileTypes={["image/*"]}
-                    name="files"
-                    labelIdle='Drag & Drop your files or <span className="filepond--label-action">Browse</span>'
-                  />
-                  <Button
-                    style={{ width: "100%" }}
-                    variant="contained"
-                    color="primary"
-                    onClick={updateUserImageHandle}
-                  >
-                    Update
-                  </Button>
-                </Grid>
-              </div>
-            }
-          />
-        </ProfileImage>
+        <ProfileImageComponent user={user} />
         <ProfileDetails>
           <div>
             <h2>{user.firstname + " " + user.lastname}</h2>
