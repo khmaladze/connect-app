a4dd3807402a43c5a256795efc47229ecd63acc2
khmaladze <khmaladze.g.7@gmail.com>
2023-10-21 21:14:41 -0700

    add send friend request route

--- backend/src/routes/friend/index.ts
+++ backend/src/routes/friend/index.ts
@@ -1,5 +1,12 @@
 import express from "express";
 import userAuthorization from "../../middleware/user-authorization";
+import * as sendFriendRequest from "./post.friend-request";
 let router = express.Router();
+
+router.post(
+  "/send_friend_request",
+  userAuthorization,
+  sendFriendRequest.businessLogic
+);
 
 export default router;
