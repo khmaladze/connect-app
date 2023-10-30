39c59161da6bcf562601de82ce2fb227b4031fd8
khmaladze <khmaladze.g.7@gmail.com>
2023-10-30 00:31:04 -0700

    add user pages

---
+++ frontend/src/pages/user/profile-page/ProfilePageStyle.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ProfilePageMain = styled.div`
+  height: auto;
+  width: 70%;
+  margin: 0 auto;
+`;
+
+export const ProfileInfoContainer = styled.div`
+  height: 385px;
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid grey;
+`;
