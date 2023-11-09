8258858e9c9bfccb549e989f46503ffb5ba45818
khmaladze <khmaladze.g.7@gmail.com>
2023-11-08 19:55:00 -0800

    backend add documentation for get send friend request

--- backend/src/routes/friend/get/get.send-request.ts
+++ backend/src/routes/friend/get/get.send-request.ts
@@ -9,6 +9,62 @@ import {
 import { CustomRequest } from "../../../middleware/user-authorization";
 import { User } from "../../../models/user/user-model";
 
+/**
+ * @swagger
+ * /api/user/friend/get_send_request:
+ *   get:
+ *     summary: Get friend send requests sent by the user.
+ *     description: Retrieve pending friend send requests sent by the user.
+ *     tags:
+ *       - Friend
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         required: true
+ *         schema:
+ *           type: string
+ *           format: Bearer jwt_token
+ *         description: The user's JWT token for authorization.
+ *     responses:
+ *       200:
+ *         description: Friend send requests retrieved successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   user:
+ *                     type: object
+ *                     properties:
+ *                       _id:
+ *                         type: string
+ *                         description: The unique ID of the user who received the send request.
+ *                       username:
+ *                         type: string
+ *                         description: The username of the user who received the send request.
+ *                       gender:
+ *                         type: string
+ *                         description: The gender of the user who received the send request.
+ *                       profileImage:
+ *                         type: string
+ *                         description: The URL of the user's profile image who received the send request.
+ *                   request:
+ *                     type: object
+ *                     properties:
+ *                       _id:
+ *                         type: string
+ *                         description: The unique ID of the friend send request.
+ *                       sender:
+ *                         type: string
+ *                         description: The sender of the friend send request.
+ *                       receiver:
+ *                         type: string
+ *                         description: The receiver of the friend send request.
+ *     security:
+ *       - BearerAuth: []
+ */
 export const businessLogic = async (req: CustomRequest, res: Response) => {
   try {
     const userProfileId: number = req.user._id;
