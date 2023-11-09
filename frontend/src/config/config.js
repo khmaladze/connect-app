da107239dd24b2afd00edb8b5ae921a149557e1b
khmaladze <khmaladze.g.7@gmail.com>
2023-11-08 19:58:26 -0800

    frontend update friend route url

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -47,7 +47,7 @@ export const API_URL = {
     },
     put: {
       friend_request_response: `${USER_API + "/friend/response"}`,
-      friend_request_remove: `${USER_API + "/friend/remove"}`,
+      friend_request_remove: `${USER_API + "/friend/remove_request"}`,
     },
   },
 };
