8615f918870ff2411c4a068222b0d3d9f778ce66
khmaladze <khmaladze.g.7@gmail.com>
2023-11-24 15:20:20 -0800

    frontend update Loading

--- frontend/src/components/loading/Loading.jsx
+++ frontend/src/components/loading/Loading.jsx
@@ -1,20 +1,21 @@
 import { CircularProgress } from "@mui/material";
 import React from "react";
+import styled from "styled-components";
 
 const Loading = () => {
   return (
-    <div
-      style={{
-        maxWidth: "1500px",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: "15px",
-      }}
-    >
+    <LoadingComponent>
       <CircularProgress />
-    </div>
+    </LoadingComponent>
   );
 };
+
+const LoadingComponent = styled.div`
+  max-width: 1500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 15px;
+`;
 
 export default Loading;
