0fcbb17d744bc6fc99623c264d6356bc4c8a138c
khmaladze <khmaladze.g.7@gmail.com>
2023-11-24 19:55:35 -0800

    frontend update friend card style

--- frontend/src/components/user/friend-page/friend-cards/friend-card/FriendCard.jsx
+++ frontend/src/components/user/friend-page/friend-cards/friend-card/FriendCard.jsx
@@ -7,11 +7,12 @@ import Typography from "@mui/material/Typography";
 import { apiRequest, userProfileImage } from "../../../../../api/user/Api";
 import { CircularProgress, MenuItem, Select } from "@mui/material";
 import { API_URL } from "../../../../../config/config";
+import styled from "styled-components";
 
 const FriendCard = ({ token }) => {
   const [loading, setLoading] = useState(true);
   const [friendList, setFriendList] = useState(false);
-  //   const [status, setStatus] = useState("Friend");
+  const [status, setStatus] = useState("Friend");
 
   useEffect(() => {
     const getFriendList = async () => {
@@ -48,51 +49,68 @@ const FriendCard = ({ token }) => {
                 height: "380px",
               }}
             >
-              <Card>
-                <CardMedia
-                  component="img"
-                  alt="User Image"
-                  height="200"
-                  image={userProfileImage(
-                    item.user.gender,
-                    item.user.profileImage
-                  )}
-                />
-                <CardContent>
-                  <Typography variant="h6" component="div">
-                    {item.user.username}
-                  </Typography>
-                  <Select
-                    disabled
-                    style={{
-                      width: "100%",
-                      marginTop: "10px",
-                    }}
-                    labelId="dropdown-label"
-                    id="dropdown"
-                    value={item.request.friend_list}
-                    // onChange={(event) => setStatus(event.target.value)}
-                  >
-                    <MenuItem
-                      key={item.request.friend_list}
+              <CardBorder borderColor={item.request.friend_list}>
+                <Card>
+                  <CardMedia
+                    component="img"
+                    alt="User Image"
+                    height="200"
+                    image={userProfileImage(
+                      item.user.gender,
+                      item.user.profileImage
+                    )}
+                  />
+                  <CardContent>
+                    <Typography variant="h6" component="div">
+                      {item.user.username}
+                    </Typography>
+                    <Select
+                      disabled
+                      style={{
+                        width: "100%",
+                        marginTop: "10px",
+                      }}
+                      labelId="dropdown-label"
+                      id="dropdown"
                       value={item.request.friend_list}
+                      // onChange={(event) => setStatus(event.target.value)}
                     >
-                      {item.request.friend_list}
-                    </MenuItem>
-                    {/* <MenuItem key={"CloseFriend"} value={"CloseFriend"}>
+                      <MenuItem
+                        key={item.request.friend_list}
+                        value={item.request.friend_list}
+                      >
+                        {item.request.friend_list}
+                      </MenuItem>
+                      {/* <MenuItem key={"CloseFriend"} value={"CloseFriend"}>
                       close friend
                     </MenuItem>
                     <MenuItem key={"Favorite"} value={"Favorite"}>
                       Favorite
                     </MenuItem> */}
-                  </Select>
-                </CardContent>
-              </Card>
+                    </Select>
+                  </CardContent>
+                </Card>
+              </CardBorder>
             </div>
           );
         })}
     </Fragment>
   );
 };
+
+const CardBorder = styled.div`
+  border: 2px solid
+    ${(props) =>
+      props.borderColor === "Friend"
+        ? "#0500ff"
+        : props.borderColor === "CloseFriend"
+        ? "#1eff1e"
+        : "#FF008A"};
+  background: white;
+  border-radius: 7px;
+  transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+`;
 
 export default FriendCard;
