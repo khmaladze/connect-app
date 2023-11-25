a073c0b846cb1e702324bd9235414bc4b951f847
khmaladze <khmaladze.g.7@gmail.com>
2023-11-24 21:03:20 -0800

    backend add user post like

---
+++ backend/src/routes/profile/get/get.user-post-liked.ts
@@ -0,0 +1,96 @@
+import { Response } from "express";
+import { customServerError } from "../../../function/server-custom-error-response";
+import { custom_server_response } from "../../../function/server-response";
+import { CustomRequest } from "../../../middleware/user-authorization";
+import { PostLike } from "../../../models/post-like/post-like-model";
+
+// Documentation
+/**
+ * @swagger
+ * /api/user/profile/check_post_like/{postId}:
+ *   get:
+ *     summary: Check if the authenticated user has liked a specific post.
+ *     description: |
+ *       This endpoint checks if the authenticated user has already liked a specific post.
+ *     tags:
+ *       - Profile
+ *     parameters:
+ *       - in: path
+ *         name: postId
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The ID of the post to check for likes.
+ *         example: "654c4ef1f69e755c68cd3079"
+ *     responses:
+ *       200:
+ *         description: Success. Indicates whether the user has liked the post.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   description: Indicates the success of the operation.
+ *                 message:
+ *                   type: string
+ *                   description: A message indicating the result.
+ *                 data:
+ *                   type: object
+ *                   description: Additional data, if needed.
+ *                   properties:
+ *                     liked:
+ *                       type: boolean
+ *                       description: Indicates whether the user has liked the post.
+ *       400:
+ *         description: Bad Request. Indicates an issue with the request parameters.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   description: Indicates the success of the operation.
+ *                 message:
+ *                   type: string
+ *                   description: A message indicating the result.
+ *       500:
+ *         description: Internal Server Error. Indicates an issue on the server.
+ *     security:
+ *       - BearerAuth: []
+ */
+
+// route message
+const routeMessage = {
+  like_already_exists: "like already exists",
+  post_dont_exists: "like don't exists",
+};
+
+export const businessLogic = async (req: CustomRequest, res: Response) => {
+  try {
+    const userProfileId: number = req.user._id;
+    const postId = req.params.postId;
+
+    const isLikeExists = await PostLike.exists({
+      post_id: postId,
+      like_author_id: userProfileId,
+    });
+
+    if (isLikeExists) {
+      return custom_server_response(
+        res,
+        200,
+        routeMessage.like_already_exists,
+        { liked: true }
+      );
+    }
+
+    return custom_server_response(res, 200, routeMessage.post_dont_exists, {
+      liked: false,
+    });
+  } catch (error) {
+    return customServerError(res, error, req);
+  }
+};
