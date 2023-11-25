2bcf873b06a3a5b0b378dc93273f884f1f8eb69e
khmaladze <khmaladze.g.7@gmail.com>
2023-11-24 19:37:59 -0800

    frontend update profile post footer

--- frontend/src/components/user/profile-component/post-component/ProfilePostFooter.jsx
+++ frontend/src/components/user/profile-component/post-component/ProfilePostFooter.jsx
@@ -1,51 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { ProfilePostFooter } from "./ProfilePostStyle";
-import { apiRequest } from "../../../../api/user/Api";
-import { API_URL } from "../../../../config/config";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import AddCommentIcon from "@mui/icons-material/AddComment";
-import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import ProfilePostFooterLike from "./ProfilePostFooterLike";
+import ProfilePostFooterComment from "./ProfilePostFooterComment";
 
 const ProfilePostFooterComponent = ({ list, postId, token }) => {
-  const [isLiked, setIsLiked] = useState(false);
-  const [isComment, setIsComment] = useState(false);
-
-  const likePost = async () => {
-    try {
-      const response = await apiRequest(
-        "POST",
-        API_URL.profile.post.like_post,
-        token,
-        {
-          post_id: postId,
-        }
-      );
-      if (response?.success) {
-        setIsLiked(true);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <ProfilePostFooter borderColor={list}>
       <div>
-        {isLiked ? (
-          <FavoriteIcon
-            onClick={() => {
-              likePost();
-            }}
-          />
-        ) : (
-          <FavoriteBorderIcon
-            onClick={() => {
-              likePost();
-            }}
-          />
-        )}
-        {isComment ? <AddCommentIcon /> : <ChatBubbleOutlineIcon />}
+        <ProfilePostFooterLike token={token} postId={postId} />
+        <ProfilePostFooterComment />
       </div>
     </ProfilePostFooter>
   );
