20b0d95055c5877c138ec07b37f1e1d12f3cb52c
khmaladze <khmaladze.g.7@gmail.com>
2023-11-25 15:06:28 -0800

    backend add remove post like

---
+++ backend/src/routes/profile/post/post.remove-post-like.ts
@@ -0,0 +1,111 @@
+import { Response } from "express";
+import { customServerError } from "../../../function/server-custom-error-response";
+import { custom_server_response } from "../../../function/server-response";
+import { CustomRequest } from "../../../middleware/user-authorization";
+import Joi from "joi";
+import { PostLike } from "../../../models/post-like/post-like-model";
+
+// Documentation
+/**
+ * @swagger
+ * /api/user/profile/remove_post_like:
+ *   post:
+ *     summary: remove post like.
+ *     description: Allow a user to like a specific post.
+ *     tags:
+ *       - Profile
+ *     security:
+ *       - BearerAuth: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               post_id:
+ *                 type: string
+ *                 description: The ID of the post to be liked.
+ *     responses:
+ *       '200':
+ *         description: Successful post like response.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   description: Indicates the success of the request.
+ *                 message:
+ *                   type: string
+ *                   description: Success message related to the request.
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     post_id:
+ *                       type: string
+ *                       description: ID of the liked post.
+ *                     like_author_id:
+ *                       type: string
+ *                       description: ID of the user who liked the post.
+ *                     _id:
+ *                       type: string
+ *                       description: Unique identifier for the post like.
+ *                     createdAt:
+ *                       type: string
+ *                       description: Date and time when the post like was created.
+ *                       format: date-time
+ *                     updatedAt:
+ *                       type: string
+ *                       description: Date and time when the post like was last updated.
+ *                       format: date-time
+ *                     __v:
+ *                       type: number
+ *                       description: Version number.
+ */
+
+// validation
+const postLikeSchema = Joi.object({
+  post_id: Joi.string().required(),
+});
+
+// route message
+const routeMessage = {
+  like_not_exists: "like not exists",
+  like_already_exists: "like already exists",
+  post_like_remove_success: "post like remove success",
+};
+
+export const businessLogic = async (req: CustomRequest, res: Response) => {
+  try {
+    const userProfileId: number = req.user._id;
+
+    // Validate request body
+    const schemaValidation = await postLikeSchema.validateAsync(req.body);
+
+    const { post_id } = req.body;
+
+    const isLikeAlreadyExists = await PostLike.exists({
+      post_id: post_id,
+      like_author_id: userProfileId,
+    });
+
+    if (!isLikeAlreadyExists) {
+      return custom_server_response(res, 400, routeMessage.like_not_exists);
+    }
+
+    await PostLike.deleteOne({
+      post_id: post_id,
+      like_author_id: userProfileId,
+    });
+
+    return custom_server_response(
+      res,
+      200,
+      routeMessage.post_like_remove_success
+    );
+  } catch (error) {
+    return customServerError(res, error, req);
+  }
+};
