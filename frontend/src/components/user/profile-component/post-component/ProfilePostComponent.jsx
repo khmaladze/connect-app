9e4e6785ff47877d23e12d50c249381554a86c83
khmaladze <khmaladze.g.7@gmail.com>
2023-11-25 22:21:07 -0800

    frontend add load more for profile posts

--- frontend/src/components/user/profile-component/post-component/ProfilePostComponent.jsx
+++ frontend/src/components/user/profile-component/post-component/ProfilePostComponent.jsx
@@ -5,54 +5,76 @@ import ProfilePost from "./ProfilePost";
 import Loading from "../../../loading/Loading";
 
 const ProfilePostComponent = ({ user }) => {
-  const [profilePosts, setProfilePosts] = useState("");
+  const [profilePosts, setProfilePosts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
 
   useEffect(() => {
     const fetchProfilePost = async () => {
-      const response = await apiRequest(
-        "GET",
-        false,
-        API_URL.profile.get.profile_post,
-        user.token
-      );
-      if (response?.success) {
-        setTimeout(() => {
-          setLoading(!loading);
-          setProfilePosts(response.data);
-        }, 500);
-      } else {
-        setTimeout(() => {
-          setLoading(!loading);
-        }, 3000);
+      try {
+        const response = await apiRequest(
+          "GET",
+          false,
+          `${API_URL.profile.get.profile_post}?page=${page}&pageSize=5`,
+          user.token
+        );
+
+        if (response?.success) {
+          setLoading(false);
+          setProfilePosts((prevPosts) => [...prevPosts, ...response.data]);
+
+          if (response.data.length < 5) {
+            setHasMore(false);
+          }
+        } else {
+          setLoading(false);
+        }
+      } catch (error) {
+        console.error("Error fetching profile posts:", error);
+        setLoading(false);
       }
     };
+
     fetchProfilePost();
-  }, []);
+
+    const handleScroll = () => {
+      // Fetch more posts when the user is near the bottom of the page
+      if (
+        window.innerHeight + window.scrollY >= document.body.offsetHeight &&
+        hasMore
+      ) {
+        setPage((prevPage) => prevPage + 1);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [page, hasMore, user.token]);
 
   return (
     <Fragment>
       {loading && <Loading />}
-      {profilePosts
-        ? profilePosts.map((item) => {
-            return (
-              <div key={item._id}>
-                <ProfilePost
-                  postId={item._id}
-                  firstname={user.firstname}
-                  lastname={user.lastname}
-                  createdAt={item.createdAt}
-                  profileImage={user.profileImage}
-                  gender={user.gender}
-                  text={item.text || ""}
-                  image={item.media.length > 0 ? item.media[0].url : ""}
-                  list={item.list}
-                  token={user.token}
-                />
-              </div>
-            );
-          })
-        : ""}
+      {profilePosts &&
+        profilePosts.map((item) => (
+          <div key={item._id}>
+            <ProfilePost
+              postId={item._id}
+              firstname={user.firstname}
+              lastname={user.lastname}
+              createdAt={item.createdAt}
+              profileImage={user.profileImage}
+              gender={user.gender}
+              text={item.text || ""}
+              image={item.media.length > 0 ? item.media[0].url : ""}
+              list={item.list}
+              token={user.token}
+            />
+          </div>
+        ))}
     </Fragment>
   );
 };
