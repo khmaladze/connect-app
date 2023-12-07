679d788e65203cc68f2d37ecc66ee130fed568c6
khmaladze <khmaladze.g.7@gmail.com>
2023-12-07 15:52:49 -0800

    backend update server route messages

--- backend/src/function/server-route-messages.ts
+++ backend/src/function/server-route-messages.ts
@@ -3,21 +3,8 @@ export const apiSuccessStatusMessage = {
   no_success: false,
 };
 
-export const userCheckMessage = {
-  user_required: "user required",
-  not_authorized: "Not authorized",
-};
-
 export const multerImageMessage = {
   image_required: "image required",
-};
-
-export const userAuthorizationMessage = {
-  user_not_found: "Error, You are not user. user not found",
-  session_not_found: "Session not found",
-  session_expired: "Session expired",
-  not_authorized: "Not authorized",
-  not_authorized_no_token: "Not authorized, no token",
 };
 
 export const serverErrorMessage = {
