c539a89782a55fd21d72bb0e64eda71e65803e88
khmaladze <khmaladze.g.7@gmail.com>
2023-12-07 23:34:45 -0800

    frontend update store/auth.js

--- frontend/src/store/auth.js
+++ frontend/src/store/auth.js
@@ -3,23 +3,18 @@ import { createSlice } from "@reduxjs/toolkit";
 // Retrieve user information from local storage or set to null if not found
 const user = JSON.parse(localStorage.getItem("user")) || null;
 
-// Define initial state for the auth slice
-const initialStateValue = {
-  user,
-};
-
 // Create the auth slice
 export const authSlice = createSlice({
   name: "auth",
-  initialState: { value: initialStateValue },
+  initialState: { user }, // Initialize with an object containing the user
   reducers: {
     // Action to update the user information upon login
     logIn: (state, action) => {
-      state.value = action.payload;
+      state.user = action.payload; // Update the user property
     },
     // Action to clear the user information upon logout
     logOut: (state) => {
-      state.value = null;
+      state.user = null; // Clear the user property
     },
   },
 });
