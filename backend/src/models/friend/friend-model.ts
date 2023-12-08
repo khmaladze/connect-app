f86f233145b5820d5309525d45ea4119955cb187
khmaladze <khmaladze.g.7@gmail.com>
2023-12-07 17:07:48 -0800

    backend update friend model

--- backend/src/models/friend/friend-model.ts
+++ backend/src/models/friend/friend-model.ts
@@ -23,20 +23,28 @@ interface UserFriendDocument extends Document {
 // Define the schema for the UserFriend model
 const userFriendSchema = new mongoose.Schema<UserFriendDocument>(
   {
+    // Reference to the user profile for which the friend list is maintained
     user_profile_id: { type: Schema.Types.ObjectId, ref: "User" },
+
+    // Array of friends, each represented by a FriendEntry
     friends: [
       {
+        // Date when the friendship was established (default is the current date)
         friends_from: { type: Date, default: Date.now },
+
+        // Type of friendship (using the FriendListType enum)
         friend_list: {
           type: String,
           enum: Object.values(FriendListType),
-          default: FriendListType.Friend,
+          default: FriendListType.Friend, // Default to "Friend" if not specified
         },
+
+        // Reference to the friend's user profile
         user_id: { type: Schema.Types.ObjectId, ref: "User" },
       },
     ],
   },
-  { timestamps: true }
+  { timestamps: true } // Include timestamps for createdAt and updatedAt
 );
 
 // Create the UserFriend model from the schema
