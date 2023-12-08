2cf29caed6ab139278bac369563bfa735e87e9bf
khmaladze <khmaladze.g.7@gmail.com>
2023-12-07 17:10:38 -0800

    backend update post-like-model.ts

--- backend/src/models/post/post-like-model.ts
+++ backend/src/models/post/post-like-model.ts
@@ -1,26 +1,30 @@
 import mongoose, { Document, Schema } from "mongoose";
 
+// Interface representing the structure of a Post Like
 export interface IPostLike extends Document {
   post_id: any;
   like_author_id: any;
 }
 
+// Schema for the Post Like model
 const postLikeSchema = new Schema<IPostLike>(
   {
     post_id: {
       type: Schema.Types.ObjectId,
-      ref: "Post",
+      ref: "Post", // Reference to the Post model
       required: true,
     },
     like_author_id: {
       type: Schema.Types.ObjectId,
-      ref: "User",
+      ref: "User", // Reference to the User model
       required: true,
     },
   },
-  { timestamps: true }
+  { timestamps: true } // Automatically add createdAt and updatedAt timestamps
 );
 
+// Model for the Post Like
 const PostLike = mongoose.model("PostLike", postLikeSchema);
 
+// Export the PostLike model
 export { PostLike };
