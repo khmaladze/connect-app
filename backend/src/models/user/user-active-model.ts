4ae0a7d4abbafdf60b2aafd495a6a3d2cced8a24
khmaladze <khmaladze.g.7@gmail.com>
2023-12-07 17:29:21 -0800

    backend update user-active-model.ts

--- backend/src/models/user/user-active-model.ts
+++ backend/src/models/user/user-active-model.ts
@@ -1,18 +1,21 @@
 import { Document, Schema, model } from "mongoose";
 
+// Enum representing different user active status types
 export enum UserActiveStatusEnum {
   Active = "Active",
-  Blocked = "blocked",
+  Blocked = "Blocked",
   LogOut = "LogOut",
 }
 
+// Interface defining the structure of the UserActive document
 export interface UserActive extends Document {
-  jwt: string;
-  status: UserActiveStatusEnum;
-  user_id: any;
-  expires: string;
+  jwt: string; // JWT token associated with the active session
+  status: UserActiveStatusEnum; // Status of the user's activity (Active, Blocked, LogOut)
+  user_id: any; // Reference to the associated User document
+  expires: string; // Expiry date and time of the active session
 }
 
+// Schema for the UserActive model
 const userActiveSchema = new Schema<UserActive>(
   {
     jwt: {
@@ -34,7 +37,8 @@ const userActiveSchema = new Schema<UserActive>(
       required: true,
     },
   },
-  { timestamps: true }
+  { timestamps: true } // Automatically add timestamps for 'createdAt' and 'updatedAt'
 );
 
+// Create the UserActive model from the schema
 export default model<UserActive>("UserActive", userActiveSchema);
