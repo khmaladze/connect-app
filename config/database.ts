46ac3b0b6f7534b3cc5a5175855b88f3bec323e3
khmaladze <khmaladze.g.7@gmail.com>
2023-12-07 16:19:37 -0800

    backend update database.ts

--- config/database.ts
+++ config/database.ts
@@ -1,9 +1,17 @@
-import mongoose from "mongoose";
+import mongoose, { ConnectOptions } from "mongoose";
 import config from "./config";
+
+interface ExtendedConnectOptions extends ConnectOptions {
+  useNewUrlParser?: boolean;
+  useUnifiedTopology?: boolean;
+}
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(config.mongodb);
+    const conn = await mongoose.connect(config.mongodb, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ExtendedConnectOptions);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
