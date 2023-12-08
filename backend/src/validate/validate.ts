730467619741f669e0371368f0948ad5297590c6
khmaladze <khmaladze.g.7@gmail.com>
2023-12-07 17:31:57 -0800

    backend update validate.ts

--- backend/src/validate/validate.ts
+++ backend/src/validate/validate.ts
@@ -1,3 +1,4 @@
+// Messages for validating environment variables
 const envValidMessages = {
   isValidEnvMessage:
     "Error: Please create and configure the '.env' file with the following variables:",
@@ -6,25 +7,22 @@ const envValidMessages = {
   port: " - PORT: The port number for the server to listen on",
   jwt_token: " - JWT_TOKEN: The secret used to sign and verify JSON web tokens",
   mongodb: " - MONGODB: The connection string for your MongoDB database",
-  cloudinary_cloud_name: " - CLOUDINARY_CLOUD_NAME: The cloudinary cloud name",
-  cloudinary_api_key: " - CLOUDINARY_API_KEY: The cloudinary API key",
-  cloudinary_api_secret: " - CLOUDINARY_API_SECRET: The cloudinary API secret",
+  cloudinary_cloud_name: " - CLOUDINARY_CLOUD_NAME: The Cloudinary cloud name",
+  cloudinary_api_key: " - CLOUDINARY_API_KEY: The Cloudinary API key",
+  cloudinary_api_secret: " - CLOUDINARY_API_SECRET: The Cloudinary API secret",
 };
 
+// Function to check if all required environment variables are defined
 const isValidEnv = () => {
-  if (
-    (process.env.NODE_ENV,
-    process.env.PORT,
-    process.env.JWT_TOKEN,
-    process.env.MONGODB,
-    process.env.CLOUDINARY_CLOUD_NAME,
-    process.env.CLOUDINARY_API_KEY,
-    process.env.CLOUDINARY_API_SECRET)
-  ) {
-    return true;
-  } else {
-    return false;
-  }
+  return (
+    process.env.NODE_ENV &&
+    process.env.PORT &&
+    process.env.JWT_TOKEN &&
+    process.env.MONGODB &&
+    process.env.CLOUDINARY_CLOUD_NAME &&
+    process.env.CLOUDINARY_API_KEY &&
+    process.env.CLOUDINARY_API_SECRET
+  );
 };
 
 // Generate detailed error message for missing environment variables
@@ -32,26 +30,19 @@ const detailMessage = () => {
   if (!isValidEnv()) {
     console.log(envValidMessages.isValidEnvMessage);
     console.log("{");
-    if (process.env.NODE_ENV == "" || !process.env.NODE_ENV)
-      console.log(envValidMessages.node_env);
-    if (process.env.PORT == "" || !process.env.PORT)
-      console.log(envValidMessages.port);
-    if (process.env.JWT_TOKEN == "" || !process.env.JWT_TOKEN)
-      console.log(envValidMessages.jwt_token);
-    if (process.env.MONGODB == "" || !process.env.MONGODB)
-      console.log(envValidMessages.mongodb);
-    if (
-      process.env.CLOUDINARY_CLOUD_NAME == "" ||
-      !process.env.CLOUDINARY_CLOUD_NAME
-    )
+
+    // Check and log each missing environment variable
+    if (!process.env.NODE_ENV) console.log(envValidMessages.node_env);
+    if (!process.env.PORT) console.log(envValidMessages.port);
+    if (!process.env.JWT_TOKEN) console.log(envValidMessages.jwt_token);
+    if (!process.env.MONGODB) console.log(envValidMessages.mongodb);
+    if (!process.env.CLOUDINARY_CLOUD_NAME)
       console.log(envValidMessages.cloudinary_cloud_name);
-    if (process.env.CLOUDINARY_API_KEY == "" || !process.env.CLOUDINARY_API_KEY)
+    if (!process.env.CLOUDINARY_API_KEY)
       console.log(envValidMessages.cloudinary_api_key);
-    if (
-      process.env.CLOUDINARY_API_SECRET == "" ||
-      !process.env.CLOUDINARY_API_SECRET
-    )
+    if (!process.env.CLOUDINARY_API_SECRET)
       console.log(envValidMessages.cloudinary_api_secret);
+
     console.log("}");
   }
 };
