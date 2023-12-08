11fc0ab0f4f5f086f4aa138e4fc41d2634bfebdd
khmaladze <khmaladze.g.7@gmail.com>
2023-12-08 00:11:11 -0800

    frontend update ProfilePostFooter.jsx

--- frontend/src/components/user/profile-component/post-component/ProfilePostFooter.jsx
+++ frontend/src/components/user/profile-component/post-component/ProfilePostFooter.jsx
@@ -7,7 +7,10 @@ const ProfilePostFooterComponent = ({ list, postId, token }) => {
   return (
     <ProfilePostFooter borderColor={list}>
       <div>
+        {/* Profile post footer like component */}
         <ProfilePostFooterLike token={token} postId={postId} />
+
+        {/* Profile post footer comment component */}
         <ProfilePostFooterComment />
       </div>
     </ProfilePostFooter>
