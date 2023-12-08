7a162a93847dcf1456484e43d4e3ea2b33789264
khmaladze <khmaladze.g.7@gmail.com>
2023-12-07 16:53:17 -0800

    backend update server-user-profile.ts

--- backend/src/function/server-user-profile.ts
+++ backend/src/function/server-user-profile.ts
@@ -1,3 +1,4 @@
+// Define a union type for Zodiac Signs
 type ZodiacSign =
   | "Aquarius"
   | "Pisces"
@@ -12,7 +13,9 @@ type ZodiacSign =
   | "Sagittarius"
   | "Capricorn";
 
+// Function to determine Zodiac Sign based on birth month and day
 export const getZodiacSign = (month: number, day: number): ZodiacSign => {
+  // Array of Zodiac Signs
   const zodiacs: ZodiacSign[] = [
     "Aquarius",
     "Pisces",
@@ -28,8 +31,8 @@ export const getZodiacSign = (month: number, day: number): ZodiacSign => {
     "Capricorn",
   ];
 
-  // Array of dates in the format of [month, day]
-  const dates: [number, number][] = [
+  // Array of Zodiac Sign start dates in the format [month, day]
+  const zodiacDates: [number, number][] = [
     [1, 20],
     [2, 19],
     [3, 21],
@@ -44,41 +47,44 @@ export const getZodiacSign = (month: number, day: number): ZodiacSign => {
     [12, 22],
   ];
 
-  const index = dates.findIndex(
+  // Find the index of the Zodiac Sign based on the birth month and day
+  const index = zodiacDates.findIndex(
     ([zodiacMonth, zodiacDay]) => month === zodiacMonth && day >= zodiacDay
   );
 
+  // Return the corresponding Zodiac Sign or default to Capricorn
   return zodiacs[index !== -1 ? index : 11];
 };
 
+// Function to check if a date is valid
 export const isValidDate = (
   year: number,
   month: number,
   day: number
 ): boolean => {
+  // Check if the month is within a valid range
   if (month < 1 || month > 12) {
     return false;
   }
 
+  // Get the number of days in the specified month
   const daysInMonth = new Date(year, month, 0).getDate();
 
+  // Check if the day is within a valid range for the specified month
   if (month === 2 && day > 29) {
     return false;
   } else if (day < 1 || day > daysInMonth) {
     return false;
   }
 
+  // If both conditions are met, the date is valid
   return true;
 };
 
-export const getDateAfter7Days = () => {
-  const currentDate = new Date(); // Get current date and time
+// Function to get the date after 7 days in ISO format
+export const getDateAfter7Days = (): string => {
+  const currentDate = new Date(); // Get the current date and time
   const futureDate = new Date(currentDate.getTime() + 7 * 24 * 60 * 60 * 1000); // Add 7 days (in milliseconds)
 
-  // const year = futureDate.getFullYear(); // Get the year
-  // const month = String(futureDate.getMonth() + 1).padStart(2, "0"); // Get the month (0-11) and pad with leading zero if necessary
-  // const day = String(futureDate.getDate()).padStart(2, "0"); // Get the day (1-31) and pad with leading zero if necessary
-
   return futureDate.toISOString();
-  // return futureDate.toISOString().split("T")[0]; // Format the date as "yyyy-mm-dd"
 };
