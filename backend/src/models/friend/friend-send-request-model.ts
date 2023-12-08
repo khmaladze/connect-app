2cb31f571db42ee720f875933163f8a676b30c2d
khmaladze <khmaladze.g.7@gmail.com>
2023-12-07 17:06:27 -0800

    backend update friend-send-request-model.ts

--- backend/src/models/friend/friend-send-request-model.ts
+++ backend/src/models/friend/friend-send-request-model.ts
@@ -1,32 +1,63 @@
-import mongoose from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
-const userFriendAddSchema = new mongoose.Schema(
+// Enum for friend list types
+enum FriendListType {
+  Friend = "Friend",
+  CloseFriend = "CloseFriend",
+  Favorite = "Favorite",
+}
+
+// Enum for friend request statuses
+enum FriendRequestStatus {
+  Pending = "pending",
+  Accepted = "accepted",
+  Rejected = "rejected",
+}
+
+// Define the interface for the document
+interface IUserFriendAdd extends Document {
+  sender: Schema.Types.ObjectId;
+  receiver: Schema.Types.ObjectId;
+  friend_list: FriendListType;
+  status: FriendRequestStatus;
+}
+
+// Define the schema for user friend requests
+const userFriendAddSchema = new mongoose.Schema<IUserFriendAdd>(
   {
+    // Sender of the friend request
     sender: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      type: Schema.Types.ObjectId,
+      ref: "User", // Reference to the User model
       required: true,
     },
+
+    // Receiver of the friend request
     receiver: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      type: Schema.Types.ObjectId,
+      ref: "User", // Reference to the User model
       required: true,
     },
+
+    // Type of friendship (using the FriendListType enum)
     friend_list: {
       type: String,
-      enum: ["Friend", "CloseFriend", "Favorite"],
-      default: "Friend",
+      enum: Object.values(FriendListType),
+      default: FriendListType.Friend,
     },
+
+    // Status of the friend request (using the FriendRequestStatus enum)
     status: {
       type: String,
-      enum: ["pending", "accepted", "rejected"],
-      default: "pending",
+      enum: Object.values(FriendRequestStatus),
+      default: FriendRequestStatus.Pending,
     },
   },
-  { timestamps: true }
+  { timestamps: true } // Include timestamps for createdAt and updatedAt
 );
 
-export const UserFriendAdd = mongoose.model(
+// Create and export the UserFriendAdd model
+export const UserFriendAdd = mongoose.model<IUserFriendAdd>(
   "UserFriendAdd",
   userFriendAddSchema
 );
