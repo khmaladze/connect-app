92c11b37abf70e33a92bafb051e9bfb445db9a93
khmaladze <khmaladze.g.7@gmail.com>
2023-12-16 03:01:48 -0800

    backend update get post likes and comment update documentation

--- backend/src/routes/profile/get/get.post-statistic.ts
+++ backend/src/routes/profile/get/get.post-statistic.ts
@@ -5,11 +5,63 @@ import { User } from "../../../models/user/user-model";
 import CommentModel from "../../../models/post/post-comment-model";
 import { customServerError } from "../../../function/server-custom-error-response";
 import { custom_server_response } from "../../../function/server-response";
+import { Post } from "../../../models/post/post-model";
+
+const routeMessages = {
+  get_post_likes_and_comment_success: "get post likes and comment success",
+  not_auth_user: "You are not authorized to view likes for this post",
+  post_not_found: "Post not found",
+};
+
+/**
+ * @swagger
+ *   /api/user/profile/posts/post-like-comment/{postId}:
+ *   get:
+ *     summary: Get likes for a post
+ *     tags:
+ *       - Profile
+ *     parameters:
+ *       - in: path
+ *         name: postId
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Returns the list of likes for the post
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Like'
+ *       404:
+ *         description: Post not found
+ *       500:
+ *         description: Internal Server Error
+ *     security:
+ *       - BearerAuth: []
+ */
 
 export const businessLogic = async (req: CustomRequest, res: Response) => {
   const postId = req.params.postId;
 
   try {
+    // Check if req.user._id is the same as the author in the Post model
+    const post = await Post.findById(postId);
+    if (!post) {
+      return custom_server_response(res, 404, routeMessages.post_not_found);
+    }
+
+    if (req.user._id.toString() !== post.author.toString()) {
+      return custom_server_response(res, 400, routeMessages.not_auth_user);
+    }
+
     // Get likes for the post
     const likes = await PostLike.find({ post_id: postId });
 
@@ -63,7 +115,7 @@ export const businessLogic = async (req: CustomRequest, res: Response) => {
     return custom_server_response(
       res,
       200,
-      "get post likes and comment success",
+      routeMessages.get_post_likes_and_comment_success,
       likesAndCommentsData
     );
   } catch (error) {
