f7906ef516e5c6b828b45ca35aeacde036c7f62e
khmaladze <khmaladze.g.7@gmail.com>
2023-12-17 20:35:49 -0800

    frontend add MainPage

--- frontend/src/pages/user/main-page/MainPage.jsx
+++ frontend/src/pages/user/main-page/MainPage.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import MainPostComponent from "../../../components/user/profile-component/post-component/MainPostComponent";
 
-const MainPage = () => {
-  return <div></div>;
+const MainPage = ({ user }) => {
+  return <MainPostComponent user={user} />;
 };
 
 export default MainPage;
