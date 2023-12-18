886e5379edf012a94d0da6b30e26a8d1169565b7
khmaladze <khmaladze.g.7@gmail.com>
2023-12-17 21:12:38 -0800

    frontend update MainPostComponent make better loading

--- frontend/src/components/user/profile-component/post-component/MainPostComponent.jsx
+++ frontend/src/components/user/profile-component/post-component/MainPostComponent.jsx
@@ -77,7 +77,19 @@ const MainPostComponent = ({ user }) => {
           />
         </div>
       ))}
-      {loading && hasMore && <Loading />}
+      {loading && hasMore && (
+        <div
+          style={{
+            height: "100%",
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Loading />
+        </div>
+      )}
     </Fragment>
   );
 };
