6826a58580e989cb089545ec2f3621dd59b75ae2
khmaladze <khmaladze.g.7@gmail.com>
2023-12-19 12:28:39 -0800

    frontend add PostHeader

--- frontend/src/components/user/post/post-header/PostHeader.jsx
+++ frontend/src/components/user/post/post-header/PostHeader.jsx
@@ -1,13 +1,8 @@
-import React, { Fragment } from "react";
-import { Avatar, Button, Grid } from "@mui/material";
-import {
-  apiRequest,
-  apiRequestType,
-  userProfileImage,
-} from "../../../../api/user/Api";
-import { API_URL } from "../../../../config/config";
+import React from "react";
+import { Avatar } from "@mui/material";
+import { userProfileImage } from "../../../../api/user/Api";
 import { PostHeader, PostHeaderContainer, PostHeaderDiv } from "../PostStyle";
-import MyModal from "../../modal/MyModal";
+import DeletePostComponent from "./DeletePostComponent";
 
 const PostHeaderComponent = ({
   gender,
@@ -21,30 +16,6 @@ const PostHeaderComponent = ({
   profilePosts,
   setProfilePosts,
 }) => {
-  const removeObjectFromArray = (arr, postIdToRemove) => {
-    return arr.filter((item) => item._id !== postIdToRemove);
-  };
-
-  const deleteUserPosthandle = async (postId, token) => {
-    if (window.location.pathname == "/profile")
-      try {
-        const response = await apiRequest(
-          apiRequestType.post,
-          true,
-          API_URL.profile.post.delete_post + postId,
-          token
-        );
-        if (response?.success) {
-          setProfilePosts(removeObjectFromArray(profilePosts, postId));
-          if (profilePosts.length === 1) {
-            window.location.reload();
-          }
-        }
-      } catch (error) {
-        console.log(error);
-      }
-  };
-
   return (
     <PostHeader borderColor={list}>
       <PostHeaderContainer>
@@ -64,41 +35,12 @@ const PostHeaderComponent = ({
         }}
       >
         <h3>{createdAt && createdAt}</h3>
-        {window.location.pathname == "/profile" && (
-          <Fragment>
-            <div style={{ width: "10px" }}></div>
-            <MyModal
-              title="Delete Post"
-              ButtonText={
-                <span className="material-symbols-outlined">delete</span>
-              }
-              body={
-                <Grid container spacing={2}>
-                  <Grid item xs={12}>
-                    <p>
-                      When you click 'Delete Post' your post will be permanently
-                      removed
-                    </p>
-                    <Button
-                      style={{
-                        width: "100%",
-                        marginTop: "10px",
-                        color: "red",
-                        borderColor: "red",
-                      }}
-                      variant="outlined"
-                      onClick={async () => {
-                        await deleteUserPosthandle(postId, token);
-                      }}
-                    >
-                      Delete Post
-                    </Button>
-                  </Grid>
-                </Grid>
-              }
-            />
-          </Fragment>
-        )}
+        <DeletePostComponent
+          postId={postId}
+          token={token}
+          profilePosts={profilePosts}
+          setProfilePosts={setProfilePosts}
+        />
       </div>
     </PostHeader>
   );
