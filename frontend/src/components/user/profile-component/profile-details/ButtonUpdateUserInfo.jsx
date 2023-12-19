0e9fd8247a5e2b268da6ce1dd10ae5922ac2bcd2
khmaladze <khmaladze.g.7@gmail.com>
2023-12-19 13:03:24 -0800

    frontend update ButtonUpdateUserInfo

--- frontend/src/components/user/profile-component/profile-details/ButtonUpdateUserInfo.jsx
+++ frontend/src/components/user/profile-component/profile-details/ButtonUpdateUserInfo.jsx
@@ -15,37 +15,41 @@ const ButtonUpdateUserInfo = ({
   token,
 }) => {
   const updateUserInfoHandle = async () => {
-    if (!selectLanguage || !selectPassion || !selectEducation) {
-      toast.error("Please fill in all the fields");
-      return;
-    }
+    try {
+      if (!selectLanguage || !selectPassion || !selectEducation) {
+        toast.error("Please fill in all the fields");
+        return;
+      }
 
-    const postData = {
-      languages: [selectLanguage],
-      passions: [selectPassion],
-      education: selectEducation,
-    };
+      const postData = {
+        languages: [selectLanguage],
+        passions: [selectPassion],
+        education: selectEducation,
+      };
 
-    const response = await apiRequest(
-      apiRequestType.put,
-      true,
-      API_URL.profile.put.updateUserProfileInfo,
-      token,
-      postData
-    );
+      const response = await apiRequest(
+        apiRequestType.put,
+        true,
+        API_URL.profile.put.updateUserProfileInfo,
+        token,
+        postData
+      );
 
-    if (response?.success) {
-      localStorage.removeItem(userLocalstorage.auth.userProfileInfoData);
-      setTimeout(() => {
-        window.location.reload();
-      }, 1000);
+      if (response?.success) {
+        localStorage.removeItem(userLocalstorage.auth.userProfileInfoData);
+        setTimeout(() => {
+          window.location.reload();
+        }, 1000);
+      }
+    } catch (error) {
+      toast.error(error.message);
     }
   };
 
   return (
     <CustomButton
       style={{ width: "100%", marginTop: "10px" }}
-      varaint={"contained"}
+      variant={"contained"}
       color={"primary"}
       onClickFuntion={updateUserInfoHandle}
       buttonText={"Update"}
