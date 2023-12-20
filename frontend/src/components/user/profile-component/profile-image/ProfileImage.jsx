5f740705047cf468558125af54ad6ab7b04f89ae
khmaladze <khmaladze.g.7@gmail.com>
2023-12-19 16:34:11 -0800

    frontend update ProfileImage

--- frontend/src/components/user/profile-component/profile-image/ProfileImage.jsx
+++ frontend/src/components/user/profile-component/profile-image/ProfileImage.jsx
@@ -13,38 +13,93 @@ import { ProfileImage } from "./ProfileImageStyle";
 import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 import ImageUploader from "../../../image-uploader/ImageUploader";
 import ButtonUpdateUserImage from "./ButtonUpdateUserImage";
+import StoryComponent from "../../story-component/StoryComponent";
+import StorySwitcher from "../../story-component/StorySwitcher";
 
 const ProfileImageComponent = ({ user }) => {
   const [image, setImage] = useState();
-  const [profileStory, setProfileStory] = useState();
-
-  const fetchProfilePost = async () => {
-    try {
-      const response = await apiRequest(
-        apiRequestType.get,
-        false,
-        `${API_URL.profile.get.profile_story}`,
-        user.token
-      );
-
-      if (response?.success) {
-        setProfileStory(response.data);
-      } else {
-        console.error("Error fetching profile story:", response?.message);
-      }
-    } catch (error) {
-      console.error("Error fetching profile story:", error);
-    }
-  };
+  const [profileStory, setProfileStory] = useState([]);
+  const [isStory, setIsStory] = useState("");
+  const [updateImageStyle, setUpdateImageStyle] = useState({
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "flex-end",
+    marginTop: "5px",
+  });
 
   useEffect(() => {
-    fetchProfilePost();
+    const fetchProfileStory = async () => {
+      try {
+        const response = await apiRequest(
+          apiRequestType.get,
+          false,
+          `${API_URL.profile.get.profile_story}`,
+          user.token
+        );
+
+        if (response?.success) {
+          setProfileStory(response.data);
+          if (
+            response.data &&
+            response.data.length > 0 &&
+            (response.data[0].list === "Favorite" ||
+              response.data[0].list === "CloseFriend" ||
+              response.data[0].list === "Friend")
+          ) {
+            setIsStory(response.data[0].list);
+            setUpdateImageStyle({
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "flex-end",
+              marginTop: "5px",
+            });
+          }
+        } else {
+          console.error("Error fetching profile story:", response?.message);
+        }
+      } catch (error) {
+        console.error("Error fetching profile story:", error);
+      }
+    };
+    fetchProfileStory();
   }, [user.token]);
 
   return (
     <Fragment>
-      <ProfileImage image={userProfileImage(user.gender, user.profileImage)}>
+      <ProfileImage
+        image={userProfileImage(user.gender, user.profileImage)}
+        isStory={isStory}
+      >
+        {isStory.length > 0 && (
+          <MyModal
+            customStyle={{
+              height: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "flex-end",
+            }}
+            ButtonText={
+              <Button
+                variant="contained"
+                color="primary"
+                startIcon={
+                  <span className="material-symbols-outlined">preview</span>
+                }
+              >
+                View Story
+              </Button>
+            }
+            title="View Story"
+            body={
+              <Grid item xs={12}>
+                <StorySwitcher data={profileStory} />
+              </Grid>
+            }
+          />
+        )}
         <MyModal
+          customStyle={updateImageStyle}
           ButtonText={
             <Button
               variant="contained"
