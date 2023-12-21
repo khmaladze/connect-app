9da46e67b0f3fe03ab61f5b96a26c7b75ae7f9ca
khmaladze <khmaladze.g.7@gmail.com>
2023-12-21 01:05:00 -0800

    frontend add storie view footer

--- frontend/src/components/user/story/StoryFooterComponent.jsx
+++ frontend/src/components/user/story/StoryFooterComponent.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import StoryFooterLike from "./StoryFooterLike";
 import { PostFooter } from "../post/PostStyle";
 
-const StoryFooterComponent = ({ token, borderColor, comment }) => {
+const StoryFooterComponent = ({ token, borderColor, comment, storyId }) => {
   return (
     <PostFooter borderColor={borderColor}>
-      <StoryFooterLike token={token} />
+      <StoryFooterLike token={token} storyId={storyId} />
     </PostFooter>
   );
 };
