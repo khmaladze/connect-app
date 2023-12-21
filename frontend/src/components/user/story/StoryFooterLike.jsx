46f9b8811c775c69503a9be437440c27a5f5f1f0
khmaladze <khmaladze.g.7@gmail.com>
2023-12-21 01:05:19 -0800

    frontend add StoryFooterLike.jsx

--- frontend/src/components/user/story/StoryFooterLike.jsx
+++ frontend/src/components/user/story/StoryFooterLike.jsx
@@ -4,29 +4,27 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import { API_URL } from "../../../config/config";
 import { apiRequest, apiRequestType } from "../../../api/user/Api";
 
-const StoryFooterLike = ({ token, postId }) => {
+const StoryFooterLike = ({ token, storyId }) => {
   const [isLiked, setIsLiked] = useState(false);
   const [count, setCount] = useState(0);
 
   const likeStorie = async () => {
     try {
-      if (token && postId) {
-        setIsLiked(true);
+      setIsLiked(true);
 
-        window.location.pathname == "/profile" && setCount(count + 1);
+      window.location.pathname === "/profile" && setCount(count + 1);
 
-        const response = await apiRequest(
-          apiRequestType.post,
-          false,
-          API_URL.post.post.like_post,
-          token,
-          {
-            post_id: postId,
-          }
-        );
-        if (response?.success) {
-          setIsLiked(true);
+      const response = await apiRequest(
+        apiRequestType.post,
+        false,
+        API_URL.story.post.like_story,
+        token,
+        {
+          story_id: storyId,
         }
+      );
+      if (response?.success) {
+        setIsLiked(true);
       }
     } catch (error) {
       console.log(error);
@@ -35,18 +33,16 @@ const StoryFooterLike = ({ token, postId }) => {
 
   const removeStorieLike = async () => {
     try {
-      if (token && postId) {
-        const response = await apiRequest(
-          apiRequestType.post,
-          false,
-          API_URL.post.post.remove_post_like,
-          token,
-          { post_id: postId }
-        );
-        if (response?.success) {
-          setIsLiked(false);
-          window.location.pathname == "/profile" && setCount(count - 1);
-        }
+      const response = await apiRequest(
+        apiRequestType.post,
+        false,
+        API_URL.story.post.remove_story_like,
+        token,
+        { story_id: storyId }
+      );
+      if (response?.success) {
+        setIsLiked(false);
+        window.location.pathname === "/profile" && setCount(count - 1);
       }
     } catch (error) {
       console.log(error);
@@ -59,7 +55,7 @@ const StoryFooterLike = ({ token, postId }) => {
         const response = await apiRequest(
           apiRequestType.get,
           false,
-          API_URL.post.get.check_post_like + "/" + postId,
+          API_URL.story.get.check_story_like + "/" + storyId,
           token
         );
         if (response?.success) {
@@ -70,11 +66,9 @@ const StoryFooterLike = ({ token, postId }) => {
         console.log(error);
       }
     };
-    if (token && postId) {
-      setTimeout(() => {
-        checkIsPostLiked();
-      }, 1000);
-    }
+    setTimeout(() => {
+      checkIsPostLiked();
+    }, 1000);
   }, [isLiked]);
 
   return (
@@ -95,7 +89,7 @@ const StoryFooterLike = ({ token, postId }) => {
               removeStorieLike();
             }}
           />
-          {window.location.pathname == "/profile" && <h4>{count}</h4>}
+          {window.location.pathname === "/profile" && <h4>{count}</h4>}
         </div>
       ) : (
         <FavoriteBorderIcon
