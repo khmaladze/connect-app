6ccfb88551c1ed1b7ab1e21996e7c438cbb0bc04
khmaladze <khmaladze.g.7@gmail.com>
2023-12-21 18:07:30 -0800

    frontend update MainPage

--- frontend/src/pages/user/main-page/MainPage.jsx
+++ frontend/src/pages/user/main-page/MainPage.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { Fragment } from "react";
 import MainPostComponent from "../../../components/user/profile-component/post-component/MainPostComponent";
+import MainStoryComponent from "../../../components/user/main-story/MainStoryComponent";
 
 const MainPage = ({ user }) => {
-  return <MainPostComponent user={user} />;
+  return (
+    <Fragment>
+      <MainStoryComponent user={user} />
+      <MainPostComponent user={user} />;
+    </Fragment>
+  );
 };
 
 export default MainPage;
