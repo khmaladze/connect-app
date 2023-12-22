aaf34f17e6d8fc4bfcc606f35133fc30969e58d4
khmaladze <khmaladze.g.7@gmail.com>
2023-12-22 13:59:21 -0800

    frontend update MainStoryDiv

---
+++ frontend/src/components/user/main-story/MainStoryDiv.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import MyModal from "../modal/MyModal";
+import StorySwitcher from "../story/StorySwitcher";
+
+const MainStoryDiv = ({ data, user }) => {
+  return (
+    <div
+      style={{
+        height: "75px",
+        width: "75px",
+        background: "white",
+        borderRadius: "50%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "5px",
+        border: `1px solid ${
+          data.list === "Friend"
+            ? "#0500ff"
+            : data.list === "CloseFriend"
+            ? "#1eff1e"
+            : "#FF008A"
+        }`,
+        marginRight: "15px",
+      }}
+      key={data._id}
+    >
+      <MyModal
+        title="View Story"
+        ButtonText={
+          <div
+            style={{
+              height: "65px",
+              width: "65px",
+              borderRadius: "50%",
+              backgroundImage: `url(${data.user.profileImage})`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+            }}
+          ></div>
+        }
+        body={
+          <StorySwitcher data={data} token={user.token} gender={user.gender} />
+        }
+      />
+    </div>
+  );
+};
+
+export default MainStoryDiv;
