fde1a73300bb115829834efd351d83f239a2e977
khmaladze <khmaladze.g.7@gmail.com>
2023-12-22 14:01:03 -0800

    frontend update MainStoryComponent

--- frontend/src/components/user/main-story/MainStoryComponent.jsx
+++ frontend/src/components/user/main-story/MainStoryComponent.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { apiRequest, apiRequestType } from "../../../api/user/Api";
 import { API_URL } from "../../../config/config";
 import Loading from "../../loading/Loading";
+import MainStoryDiv from "./MainStoryDiv";
 
 const MainStoryComponent = ({ user }) => {
   const [story, setStory] = useState([]);
@@ -30,12 +31,43 @@ const MainStoryComponent = ({ user }) => {
   }, [user.token]);
 
   return (
-    <div style={{ maxWidth: "700px", margin: "0 auto" }}>
-      {story &&
-        story.map((item) => {
-          return item._id;
-        })}
-      {loading && <Loading />}
+    <div
+      style={{
+        width: "100",
+        margin: "0 auto",
+        overflowX: "auto",
+        marginBottom: "15px",
+      }}
+    >
+      <div
+        style={{
+          maxWidth: "700px",
+          margin: "0 auto",
+          overflowX: "auto",
+        }}
+      >
+        <div
+          style={{
+            maxWidth: "700px",
+            margin: "0 auto",
+            background: "white",
+            height: "85px",
+            borderRadius: "15px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-start",
+            padding: "10px",
+          }}
+        >
+          {story &&
+            story.length > 0 &&
+            story.map((storyItem) => (
+              <MainStoryDiv key={storyItem._id} data={storyItem} user={user} />
+            ))}
+          {loading && <Loading />}
+          {story && story.length == 0 && <h4>NO STORY.</h4>}
+        </div>
+      </div>
     </div>
   );
 };
