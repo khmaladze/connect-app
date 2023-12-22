a04a39a6ce3812b8aef88339a38b3419c261ef33
khmaladze <khmaladze.g.7@gmail.com>
2023-12-21 18:12:00 -0800

    backend add new profile routes

--- backend/src/routes/profile/index.ts
+++ backend/src/routes/profile/index.ts
@@ -9,6 +9,8 @@ import * as getUserPost from "./get/get.user-post";
 import * as getUserStory from "./get/get.user-story";
 import * as deleteUserPost from "./post/post.post-delete";
 import * as getPostLikesAndComments from "./get/get.post-statistic";
+import * as deleteUserStory from "./post/post.story-delete";
+import * as getStoryLikesAndComments from "./get/get.story-statistic";
 import uploadImageToServer from "../../function/server-upload-image";
 import uploadToServer from "../../function/server-upload-image-video";
 let router = express.Router();
@@ -34,16 +36,26 @@ router.post(
 router.post(
   "/add_story",
   userAuthorization,
-  uploadImageToServer,
+  uploadToServer,
   userStory.businessLogic
 );
 router.get("/posts", userAuthorization, getUserPost.businessLogic);
 router.get("/story", userAuthorization, getUserStory.businessLogic);
 router.post("/post/:postId", userAuthorization, deleteUserPost.businessLogic);
+router.post(
+  "/story/:storyId",
+  userAuthorization,
+  deleteUserStory.businessLogic
+);
 router.get(
   "/post-like-comment/:postId",
   userAuthorization,
   getPostLikesAndComments.businessLogic
+);
+router.get(
+  "/story-like-comment/:storyId",
+  userAuthorization,
+  getStoryLikesAndComments.businessLogic
 );
 
 export default router;
