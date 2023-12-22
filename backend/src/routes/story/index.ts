cc231c4327e91d3a89e10f1d92e88350f01034ab
khmaladze <khmaladze.g.7@gmail.com>
2023-12-22 14:31:41 -0800

    backend add new route for story

--- backend/src/routes/story/index.ts
+++ backend/src/routes/story/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import userAuthorization from "../../middleware/user-authorization";
 import * as storieUserLike from "../story/post/post.user-story-like";
+import * as storieUserStoryView from "../story/post/post.story-view";
 import * as getStoryUserLike from "./get/get.user-story-liked";
 import * as removeUserStoryLike from "./post/post.remove-story-like";
 import * as addStoryComment from "./post/post.add-story-comment";
@@ -11,6 +12,11 @@ let router = express.Router();
 
 router.get("/", userAuthorization, getUserFriendsStory.businessLogic);
 router.post("/like_story", userAuthorization, storieUserLike.businessLogic);
+router.post(
+  "/story_view",
+  userAuthorization,
+  storieUserStoryView.businessLogic
+);
 router.get(
   "/check_story_like/:storyId",
   userAuthorization,
