dc3582be4887d09584776791b0927cdd002d82e5
khmaladze <khmaladze.g.7@gmail.com>
2023-12-24 09:31:39 -0800

    frontend add SettingsPage

--- frontend/src/pages/user/settings-page/SettingsPage.jsx
+++ frontend/src/pages/user/settings-page/SettingsPage.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import ActicityLog from "../../../components/user/settings/ActicityLog";
+import UpdatePassword from "../../../components/user/settings/UpdatePassword";
 
-const SettingsPage = () => {
-  return <div>SettingsPage</div>;
+const SettingsPage = ({ user }) => {
+  return (
+    <div
+      style={{
+        height: "100%",
+        padding: "10px",
+        flexDirection: "column",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          height: "30px",
+          width: "100%",
+          margin: "0 auto",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <ActicityLog user={user} />
+      </div>
+      <div
+        style={{
+          marginTop: "30px",
+          height: "30px",
+          maxWidth: "500px",
+          margin: "30px auto",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <UpdatePassword user={user} />
+      </div>
+    </div>
+  );
 };
 
 export default SettingsPage;
