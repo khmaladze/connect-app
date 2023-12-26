18b0030f7ec51b9ae5c706fbbfda930dc8392f20
khmaladze <khmaladze.g.7@gmail.com>
2023-12-26 12:53:02 -0800

    frontend update

--- frontend/src/components/user/friend-page/friend-cards/get-request/GetRequest.jsx
+++ frontend/src/components/user/friend-page/friend-cards/get-request/GetRequest.jsx
@@ -4,7 +4,7 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { CircularProgress, MenuItem, Select } from "@mui/material";
+import { CircularProgress } from "@mui/material";
 import {
   apiRequest,
   apiRequestType,
