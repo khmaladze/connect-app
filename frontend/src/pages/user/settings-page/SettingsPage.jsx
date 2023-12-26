7befbc8b1b349ab33698bbc70fcc01d40e9002a8
khmaladze <khmaladze.g.7@gmail.com>
2023-12-26 12:24:09 -0800

    frontend update SettingPage Style

--- frontend/src/pages/user/settings-page/SettingsPage.jsx
+++ frontend/src/pages/user/settings-page/SettingsPage.jsx
@@ -1,45 +1,18 @@
 import React from "react";
 import ActicityLog from "../../../components/user/settings/ActicityLog";
 import UpdatePassword from "../../../components/user/settings/UpdatePassword";
+import { SettingsItem, SettingsMainDiv } from "./SettingsPageStyle";
 
 const SettingsPage = ({ user }) => {
   return (
-    <div
-      style={{
-        height: "100%",
-        padding: "10px",
-        flexDirection: "column",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <div
-        style={{
-          height: "30px",
-          width: "100%",
-          margin: "0 auto",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+    <SettingsMainDiv>
+      <SettingsItem>
         <ActicityLog user={user} />
-      </div>
-      <div
-        style={{
-          marginTop: "30px",
-          height: "30px",
-          maxWidth: "500px",
-          margin: "30px auto",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+      </SettingsItem>
+      <SettingsItem>
         <UpdatePassword user={user} />
-      </div>
-    </div>
+      </SettingsItem>
+    </SettingsMainDiv>
   );
 };
 
