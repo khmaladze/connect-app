087e237a03dc81b580eba1baf3ea7451510c40e6
khmaladze <khmaladze.g.7@gmail.com>
2023-12-25 17:04:29 -0800

    frontend update ProfilePageStyle

--- frontend/src/pages/user/profile-page/ProfilePageStyle.jsx
+++ frontend/src/pages/user/profile-page/ProfilePageStyle.jsx
@@ -4,6 +4,10 @@ export const ProfilePageMain = styled.div`
   height: auto;
   width: 70%;
   margin: 0 auto;
+
+  @media screen and (max-width: 750px) {
+    width: 100%;
+  }
 `;
 
 export const ProfileInfoContainer = styled.div`
@@ -14,4 +18,17 @@ export const ProfileInfoContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   border-bottom: 1px solid grey;
+
+  @media screen and (max-width: 750px) {
+    width: 97%;
+  }
+
+  @media screen and (max-width: 500px) {
+    margin-top: 30px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: column;
+    height: 570px;
+  }
 `;
