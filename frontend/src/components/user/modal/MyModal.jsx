0ed143fd217ca456c9d4f28f8f66de08aeb4abc6
khmaladze <khmaladze.g.7@gmail.com>
2024-01-04 16:17:23 -0800

    frontend update MyModal.jsx

--- frontend/src/components/user/modal/MyModal.jsx
+++ frontend/src/components/user/modal/MyModal.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import Button from "@mui/material/Button";
-import Modal from "@mui/material/Modal";
+
+const Modal = lazy(() => import("@mui/material/Modal"));
 
 const MyModal = ({
   title,
@@ -25,49 +26,51 @@ const MyModal = ({
       <div style={{ cursor: "pointer" }} onClick={handleOpen}>
         {ButtonText}
       </div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-title"
-        aria-describedby="modal-description"
-      >
-        <div
-          style={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            backgroundColor: "white",
-            padding: "20px",
-            width: "97%",
-            maxWidth: modalWidth,
-          }}
+      <Suspense fallback={null}>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-title"
+          aria-describedby="modal-description"
         >
-          <h2 id="modal-title">{title}</h2>
           <div
             style={{
-              height: "20px",
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              backgroundColor: "white",
+              padding: "20px",
+              width: "97%",
+              maxWidth: modalWidth,
             }}
-          ></div>
-          <p id="modal-description">{description}</p>
-          <div
-            style={{
-              height: "20px",
-            }}
-          ></div>
-          {body}
-          <div
-            style={{
-              height: "20px",
-            }}
-          ></div>
-          <Button variant="contained" color="primary" onClick={handleClose}>
-            Close Modal
-          </Button>
-        </div>
-      </Modal>
+          >
+            <h2 id="modal-title">{title}</h2>
+            <div
+              style={{
+                height: "20px",
+              }}
+            ></div>
+            <p id="modal-description">{description}</p>
+            <div
+              style={{
+                height: "20px",
+              }}
+            ></div>
+            {body}
+            <div
+              style={{
+                height: "20px",
+              }}
+            ></div>
+            <Button variant="contained" color="primary" onClick={handleClose}>
+              Close Modal
+            </Button>
+          </div>
+        </Modal>
+      </Suspense>
     </div>
   );
 };
 
-export default MyModal;
+export default React.memo(MyModal);
