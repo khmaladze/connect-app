63b0b9cf4c5befa4e10b6a89f647dcb932a51b2f
khmaladze <khmaladze.g.7@gmail.com>
2024-01-04 17:13:20 -0800

    frontend update ProfilePost.jsx

--- frontend/src/components/user/post/ProfilePost.jsx
+++ frontend/src/components/user/post/ProfilePost.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { PostContainer, PostDiv } from "./PostStyle";
 import { apiRequest, apiRequestType } from "../../../api/user/Api";
 import { API_CONTENT_TYPE_LIST, API_URL } from "../../../config/config";
@@ -8,61 +8,69 @@ import ProfilePostBodyComponent from "./post-body/PostBody";
 import PostFooterComponent from "./post-footer/PostFooter";
 import PostCommentComponent from "./post-footer/comment/PostCommentComponent";
 
-const ProfilePost = ({
-  postId,
-  firstname,
-  lastname,
-  text,
-  media,
-  profileImage,
-  createdAt,
-  gender,
-  list,
-  token,
-  profilePosts,
-  setProfilePosts,
-  postedUserId,
-}) => {
-  const [commentsData, setCommentsData] = useState([]);
-  const [isOpenCommentField, setIsOpenCommentField] = useState(false);
-  const [commentText, setCommentText] = useState("");
-  const [isCommentDataFetched, setIsCommentDataFetched] = useState(false);
-  const [userAlreadyComment, setIsUserAlreadyComment] = useState(false);
-
-  const toggleComment = () => {
-    if (userAlreadyComment === false) {
-      setIsOpenCommentField(!isOpenCommentField);
-    }
+const debounce = (func, delay) => {
+  let timeoutId;
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func.apply(this, args), delay);
   };
+};
 
-  const handleCommentSubmit = async () => {
-    try {
-      if (commentText) {
-        const response = await apiRequest(
-          apiRequestType.post,
-          false,
-          API_URL.post.post.add_comment,
-          token,
-          { comment: String(commentText), post_id: String(postId) },
-          API_CONTENT_TYPE_LIST.application_json
-        );
+const ProfilePost = React.memo(
+  ({
+    postId,
+    firstname,
+    lastname,
+    text,
+    media,
+    profileImage,
+    createdAt,
+    gender,
+    list,
+    token,
+    profilePosts,
+    setProfilePosts,
+    postedUserId,
+  }) => {
+    const [commentsData, setCommentsData] = useState([]);
+    const [isOpenCommentField, setIsOpenCommentField] = useState(false);
+    const [commentText, setCommentText] = useState("");
+    const [isCommentDataFetched, setIsCommentDataFetched] = useState(false);
+    const [userAlreadyComment, setIsUserAlreadyComment] = useState(false);
 
-        if (response?.success) {
-          setIsCommentDataFetched(false);
-          setIsOpenCommentField(false);
-          setIsUserAlreadyComment(true);
-          setCommentText("");
-        }
-      } else {
-        toast.error("Please add text");
+    const toggleComment = useCallback(() => {
+      if (!userAlreadyComment) {
+        setIsOpenCommentField(!isOpenCommentField);
       }
-    } catch (error) {
-      console.error("Error adding comment:", error);
-    }
-  };
+    }, [userAlreadyComment, isOpenCommentField]);
 
-  useEffect(() => {
-    const fetchComments = async () => {
+    const handleCommentSubmit = useCallback(async () => {
+      try {
+        if (commentText) {
+          const response = await apiRequest(
+            apiRequestType.post,
+            false,
+            API_URL.post.post.add_comment,
+            token,
+            { comment: String(commentText), post_id: String(postId) },
+            API_CONTENT_TYPE_LIST.application_json
+          );
+
+          if (response?.success) {
+            setIsCommentDataFetched(false);
+            setIsOpenCommentField(false);
+            setIsUserAlreadyComment(true);
+            setCommentText("");
+          }
+        } else {
+          toast.error("Please add text");
+        }
+      } catch (error) {
+        console.error("Error adding comment:", error);
+      }
+    }, [commentText, postId, token]);
+
+    const fetchComments = useCallback(async () => {
       try {
         const response = await apiRequest(
           apiRequestType.get,
@@ -80,78 +88,86 @@ const ProfilePost = ({
       } catch (error) {
         console.log(error);
       }
-    };
+    }, [postId, token]);
 
-    if (isCommentDataFetched === false) {
-      setTimeout(() => {
-        fetchComments();
-      }, 1000);
-      setIsCommentDataFetched(true);
-    }
-  }, [postId, token, commentsData, isCommentDataFetched]);
+    const fetchCommentsDebounced = useCallback(debounce(fetchComments, 1000), [
+      fetchComments,
+    ]);
 
-  const deleteUserPostCommenthandle = async (postId, token) => {
-    try {
-      const response = await apiRequest(
-        apiRequestType.post,
-        true,
-        `${API_URL.post.post.delete_post_comment + "/" + postId}`,
-        token
-      );
-      if (response?.success) {
-        setIsOpenCommentField(false);
-        setIsUserAlreadyComment(false);
-        setCommentText("");
-        setCommentsData(false);
+    useEffect(() => {
+      if (isCommentDataFetched === false) {
+        fetchCommentsDebounced();
+        setIsCommentDataFetched(true);
       }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    }, [
+      postId,
+      token,
+      commentsData,
+      isCommentDataFetched,
+      fetchCommentsDebounced,
+    ]);
 
-  return (
-    <PostContainer>
-      <PostDiv borderColor={list}>
-        {/* Profile post header */}
-        <PostHeaderComponent
-          gender={gender}
-          postedUserId={postedUserId}
-          profileImage={profileImage}
-          firstname={firstname}
-          lastname={lastname}
-          createdAt={createdAt.slice(0, 10)}
-          list={list}
-          postId={postId}
-          token={token}
-          profilePosts={profilePosts}
-          setProfilePosts={setProfilePosts}
-        />
+    const deleteUserPostCommenthandle = useCallback(async () => {
+      try {
+        const response = await apiRequest(
+          apiRequestType.post,
+          true,
+          `${API_URL.post.post.delete_post_comment + "/" + postId}`,
+          token
+        );
+        if (response?.success) {
+          setIsOpenCommentField(false);
+          setIsUserAlreadyComment(false);
+          setCommentText("");
+          setCommentsData(false);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }, [postId, token]);
 
-        {/* Profile post body */}
-        <ProfilePostBodyComponent
-          text={text ? text : ""}
-          media={media ? media : ""}
-        />
+    return (
+      <PostContainer>
+        <PostDiv borderColor={list}>
+          {/* Profile post header */}
+          <PostHeaderComponent
+            gender={gender}
+            postedUserId={postedUserId}
+            profileImage={profileImage}
+            firstname={firstname}
+            lastname={lastname}
+            createdAt={createdAt.slice(0, 10)}
+            list={list}
+            postId={postId}
+            token={token}
+            profilePosts={profilePosts}
+            setProfilePosts={setProfilePosts}
+          />
 
-        {/* Profile post footer */}
-        <PostFooterComponent
-          list={list}
-          postId={postId}
-          token={token}
-          gender={gender}
-          isOpenCommentField={isOpenCommentField}
-          setIsOpenCommentField={setIsOpenCommentField}
-          commentText={commentText}
-          setCommentText={setCommentText}
-          isCommentDataFetched={isCommentDataFetched}
-          setIsCommentDataFetched={setIsCommentDataFetched}
-          userAlreadyComment={userAlreadyComment}
-          handleCommentSubmit={handleCommentSubmit}
-          toggleComment={toggleComment}
-        />
-        {commentsData &&
-          commentsData.map((comment) => {
-            return (
+          {/* Profile post body */}
+          <ProfilePostBodyComponent
+            text={text ? text : ""}
+            media={media ? media : ""}
+          />
+
+          {/* Profile post footer */}
+          <PostFooterComponent
+            list={list}
+            postId={postId}
+            token={token}
+            gender={gender}
+            isOpenCommentField={isOpenCommentField}
+            setIsOpenCommentField={setIsOpenCommentField}
+            commentText={commentText}
+            setCommentText={setCommentText}
+            isCommentDataFetched={isCommentDataFetched}
+            setIsCommentDataFetched={setIsCommentDataFetched}
+            userAlreadyComment={userAlreadyComment}
+            handleCommentSubmit={handleCommentSubmit}
+            toggleComment={toggleComment}
+          />
+          {commentsData &&
+            commentsData.map((comment) => (
               <PostCommentComponent
                 key={comment._id}
                 comment={comment}
@@ -162,11 +178,11 @@ const ProfilePost = ({
                 profileImage={comment.author_profileImage}
                 deleteUserPostCommenthandle={deleteUserPostCommenthandle}
               />
-            );
-          })}
-      </PostDiv>
-    </PostContainer>
-  );
-};
+            ))}
+        </PostDiv>
+      </PostContainer>
+    );
+  }
+);
 
 export default ProfilePost;
