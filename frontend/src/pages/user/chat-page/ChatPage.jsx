c7b32fe74c187c7a4362b0f1389c1ff57453e35e
khmaladze <khmaladze.g.7@gmail.com>
2024-01-12 17:53:10 -0800

    frontend update ChatPage

--- frontend/src/pages/user/chat-page/ChatPage.jsx
+++ frontend/src/pages/user/chat-page/ChatPage.jsx
@@ -51,7 +51,18 @@ const ChatPage = ({ user }) => {
       }}
     >
       {loading ? (
-        <Loading />
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            minHeight: "300px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Loading />
+        </div>
       ) : (
         <List>
           {users.map((userData) => (
