8ce1c574fe3cc08837b7d21b5e84cb74b7d585b0
khmaladze <khmaladze.g.7@gmail.com>
2024-02-17 16:30:24 -0800

    frontend update WelcomePage

--- frontend/src/components/welcome-page/WelcomeNotification.jsx
+++ frontend/src/components/welcome-page/WelcomeNotification.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import MyModal from "../user/modal/MyModal";
 import { StyledDisclaimer } from "../../pages/guest/about/About";
 import { Typography } from "@mui/material";
@@ -8,13 +8,19 @@ const WelcomeNotification = () => {
     <MyModal
       title="Notification"
       description={
-        <StyledDisclaimer>
-          <Typography variant="body1" paragraph>
-            This is a portfolio project intended for demonstration purposes
-            only. It is not meant for real-world use but rather to showcase my
-            skills to potential recruiters and employers.
-          </Typography>
-        </StyledDisclaimer>
+        <Fragment>
+          <StyledDisclaimer>
+            <Typography variant="body1" paragraph>
+              This is a portfolio project intended for demonstration purposes
+              only. It is not meant for real-world use but rather to showcase my
+              skills to potential recruiters and employers.
+            </Typography>
+          </StyledDisclaimer>
+          <StyledDisclaimer>
+            PLEASE NOTE: THIS PROJECT IS CURRENTLY UNDER DEVELOPMENT AND
+            UNDERGOING CONTINUOUS IMPROVEMENT.
+          </StyledDisclaimer>
+        </Fragment>
       }
       openValue={true}
     />
