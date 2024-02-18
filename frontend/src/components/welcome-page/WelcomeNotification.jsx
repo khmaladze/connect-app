f2235402ff1a1355ebc16cfc143977db35578248
khmaladze <khmaladze.g.7@gmail.com>
2024-02-17 18:48:26 -0800

    frontend update WelcomeNotification.jsx

--- frontend/src/components/welcome-page/WelcomeNotification.jsx
+++ frontend/src/components/welcome-page/WelcomeNotification.jsx
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react";
 import MyModal from "../user/modal/MyModal";
 import { StyledDisclaimer } from "../../pages/guest/about/About";
-import { Typography } from "@mui/material";
 
 const WelcomeNotification = () => {
   return (
@@ -10,11 +9,9 @@ const WelcomeNotification = () => {
       description={
         <Fragment>
           <StyledDisclaimer>
-            <Typography variant="body1" paragraph>
-              This is a portfolio project intended for demonstration purposes
-              only. It is not meant for real-world use but rather to showcase my
-              skills to potential recruiters and employers.
-            </Typography>
+            This is a portfolio project intended for demonstration purposes
+            only. It is not meant for real-world use but rather to showcase my
+            skills to potential recruiters and employers.
           </StyledDisclaimer>
           <StyledDisclaimer>
             PLEASE NOTE: THIS PROJECT IS CURRENTLY UNDER DEVELOPMENT AND
