d655fe285dbfc261820046d86138d1483ef2c729
khmaladze <khmaladze.g.7@gmail.com>
2024-02-22 12:30:16 -0800

    backend update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,6 +14,11 @@ import userFriendProfileRoutes from "./routes/userprofile/index";
 import { v2 as cloudinary } from "cloudinary";
 import swaggerSpec from "./swagger";
 import swaggerUi from "swagger-ui-express";
+import mongoSanitize from "express-mongo-sanitize";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import hpp from "hpp";
+import path from "path";
 
 // Check if environment variables are provided
 if (isValidEnv()) {
@@ -22,6 +27,27 @@ if (isValidEnv()) {
 
   // Initialize the app
   const app: Application = express();
+
+  app.use(mongoSanitize());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 300, // limit each IP to 100 requests per windowMs
+    })
+  );
+  app.use(helmet());
+  // Set Content Security Policy
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+        styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
+        imgSrc: ["'self'", "data:", "https://res.cloudinary.com"],
+      },
+    })
+  );
+  app.use(hpp());
 
   // Set up Cloudinary configuration
   cloudinary.config({
@@ -49,6 +75,16 @@ if (isValidEnv()) {
   // Serve API documentation only in development environment
   if (config.node_env === "development") {
     app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  }
+
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../../../frontend/build")));
+
+    app.get("*", (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, "../../../frontend/build", "index.html")
+      )
+    );
   }
 
   // Start the server
